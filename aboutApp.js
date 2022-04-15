a3629540eb49d41c83cc8525047c48013b9b99c2
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-04-15 04:53:08 -0300

    fix js linters errors

--- aboutApp.js
+++ aboutApp.js
@@ -1,21 +1,20 @@
 // mobile menu about section
-const hamburgerIcon2 = document.getElementById("about-image");
-const xIcon2 = document.getElementById("about-close");
-const navbar2 = document.getElementById("about-appear");
-const linkMenu = document.querySelectorAll(".menu-li");
+const hamburgerIcon2 = document.getElementById('about-image');
+const xIcon2 = document.getElementById('about-close');
+const navbar2 = document.getElementById('about-appear');
+const linkMenu = document.querySelectorAll('.menu-li');
 
-
-hamburgerIcon2.addEventListener("click", () => {
-  navbar2.classList.toggle("menu-open");
-  hamburgerIcon2.classList.toggle("disappear");
+hamburgerIcon2.addEventListener('click', () => {
+  navbar2.classList.toggle('menu-open');
+  hamburgerIcon2.classList.toggle('disappear');
 });
 
 const closeMenu2 = () => {
-  navbar2.classList.remove("menu-open");
-  hamburgerIcon2.classList.remove("disappear");
+  navbar2.classList.remove('menu-open');
+  hamburgerIcon2.classList.remove('disappear');
 };
-xIcon2.addEventListener("click", closeMenu2);
+xIcon2.addEventListener('click', closeMenu2);
 
 linkMenu.forEach((link) => {
-  link.addEventListener("click", closeMenu2);
+  link.addEventListener('click', closeMenu2);
 });
