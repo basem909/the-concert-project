5ce3ef2f50931b1f8f50068f1c5548fb761a87d4
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-04-15 04:42:32 -0300

    fix the responsiveness of the menu of mobile version index.html

---
+++ aboutApp.js
@@ -0,0 +1,21 @@
+// mobile menu about section
+const hamburgerIcon2 = document.getElementById("about-image");
+const xIcon2 = document.getElementById("about-close");
+const navbar2 = document.getElementById("about-appear");
+const linkMenu = document.querySelectorAll(".menu-li");
+
+
+hamburgerIcon2.addEventListener("click", () => {
+  navbar2.classList.toggle("menu-open");
+  hamburgerIcon2.classList.toggle("disappear");
+});
+
+const closeMenu2 = () => {
+  navbar2.classList.remove("menu-open");
+  hamburgerIcon2.classList.remove("disappear");
+};
+xIcon2.addEventListener("click", closeMenu2);
+
+linkMenu.forEach((link) => {
+  link.addEventListener("click", closeMenu2);
+});
